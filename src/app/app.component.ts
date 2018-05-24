b2ed76ea2271bb6ceb1a0ef9a0dde8415f3f2c6b
BALDE TMS <sambafaf@comenva.com>
2018-05-24 16:06:39 +0000

    Upload project to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Like It Or Not Blog';
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  posts = [
+    {
+      title : "Jordan & House = false",
+      content :"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      loveIts : "50",
+      createdAt : new Date(2018,5,5,10,20)
+    },
+    {
+      title : "Micheals is here",
+      content :"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      loveIts : 2,
+      createdAt : new Date(2018,5,24,21,20)
+    }
+  ]
+
+
 }
---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+
+
+export class PostListItemComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveIts: number;
+  @Input() createdAt: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onLike(){
+    this.loveIts += 1;    
+  }
+  onLikeNot(){
+    this.loveIts -= 1;    
+  }
+
+}
